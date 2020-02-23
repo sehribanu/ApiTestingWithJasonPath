a64e2b8e37e9e536e21ee7037ef91118a70b217e
sehribanu <sehribanbanu@gmail.com>
2020-02-23 22:18:10 +0000

    committed project files

--- src/test/java/com/testings/tests/TestBase.java
+++ src/test/java/com/testings/tests/TestBase.java
@@ -1,17 +1,11 @@
 package com.testings.tests;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.testings.utilities.ConfigurationReader;
 import io.restassured.RestAssured;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class TestBase {
-    protected ExtentReports report;
-    protected ExtentHtmlReporter htmlReporter;
-    protected ExtentTest extentLogger;
+
 
     @BeforeTest
     public void setUpTest(){
